d86a0e0639200b53b977b8f900fd122efd36afe4
Dror Matalon <dror@matal.com>
2022-12-13 22:49:28 -0800

    Print the handle of the person commenting.

--- comments.py
+++ comments.py
@@ -4,7 +4,6 @@ import html
 class HackerNewsComments:
     # Set the base URL for the Hacker News API
     HACKER_NEWS_API_URL = 'https://hacker-news.firebaseio.com/v0'
-
     def __init__(self, max_comments):
         self.max_comments = max_comments
 
@@ -20,7 +19,12 @@ class HackerNewsComments:
 
         # Print the text of the thread
         if 'text' in thread and thread['text']:
-            print("\t" * indent, html.unescape(thread['text']))
+            # Fetch the user name from the API
+            user_response = requests.get(f'{HackerNewsComments.HACKER_NEWS_API_URL}/user/{thread["by"]}.json')
+            user = user_response.json()
+
+            # Print the user name and the comment
+            print("\t" * indent, f'{user["id"]} says:', html.unescape(thread['text']))
 
         # Check if the thread has any comments
         if 'kids' in thread and thread['kids']:
@@ -30,5 +34,6 @@ class HackerNewsComments:
 
 # Fetch the comments for the thread with id 33961106
 # and print them in a hierarchical manner
-hn_comments = HackerNewsComments(5)
+
+hn_comments = HackerNewsComments(100)
 hn_comments.fetch_comments(33961106)
